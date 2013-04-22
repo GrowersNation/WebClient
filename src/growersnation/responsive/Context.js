43188abdbf21cc7879567997eed57755d13446cb
Jeremy Brooks <jeremy_brooks@hotmail.com>
2013-04-22 14:07:45 +0100

    styles and robotlegs type wiring changes added
    Created a context for maintaining a single point of loading for each
    section of the site in one place.
    
    Made styles a linked sheet rather than styles in the index page.

---
+++ src/growersnation/responsive/Context.js
@@ -0,0 +1,13 @@
+define(
+	["dojo/_base/declare",
+	 "gn/responsive/location/MyLocation"],
+	
+	function (declare,
+			  MyLocation){
+		return declare(null, {
+			constructor: function(){
+				new MyLocation();
+			}
+		});
+	}
+)
--- src/growersnation/responsive/location/MyLocation.js
+++ src/growersnation/responsive/location/MyLocation.js
@@ -3,40 +3,50 @@
  */
 define(
 	["dojo/dom-construct",
-	 "dojo/request"],
+	 "dojo/request",
+	 "dojo/_base/declare"],
 	
 	function(domConstruct, 
-			 request){
+			 request,
+			 declare){
 		
 		var placesUri = "/maps/api/place/autocomplete/json?&sensor=true&key=AIzaSyAdxFxJZ5P0VdgGB3okJ9GoiQ5ebT2FFYM";
 		var locInput = "<form>Location: <input id='location' type='text' name='location'><input type='submit' value='Submit'></form>";
 		
-		domConstruct.empty("main");
-		var input = domConstruct.place(locInput, "main");
-		document.getElementById("location").onkeyup = function(args){
-			var uri = placesUri + "&input=" + document.getElementById("location").value;
-			request.get(uri, {
-				handleAs: "text"
-			}).then(
-				function(data){
-					var result = JSON.parse(data);
-					var locNames = "";
-					
-					for (var attribute in result){
-						if (result.hasOwnProperty(attribute) === true && attribute === "predictions"){
-							for (var index = 0; index < result[attribute].length; index++){
-								locNames += "<p>" + result[attribute][index].description + "</p>";
+		return declare(null, {
+			constructor: function(){
+				
+				//	summary
+				//		adds listener and elements liked to location search results
+				
+				domConstruct.empty("main");
+				var input = domConstruct.place(locInput, "main");
+				document.getElementById("location").onkeyup = function(args){
+					var uri = placesUri + "&input=" + document.getElementById("location").value;
+					request.get(uri, {
+						handleAs: "text"
+					}).then(
+						function(data){
+							var result = JSON.parse(data);
+							var locNames = "";
+							
+							for (var attribute in result){
+								if (result.hasOwnProperty(attribute) === true && attribute === "predictions"){
+									for (var index = 0; index < result[attribute].length; index++){
+										locNames += "<p><a href=''>" + result[attribute][index].description + "</a></p>";
+									}
+								}
 							}
+							
+							document.getElementById("test").innerHTML = locNames;
+						},
+						function(error){
+							document.getElementById("test").innerHTML = error;
 						}
-					}
-					
-					document.getElementById("test").innerHTML = locNames;
-				},
-				function(error){
-					document.getElementById("test").innerHTML = error;
+					);
+					return false;
 				}
-			);
-			return false;
-		}
+			}
+		});
 	}
 )
