4f3f4def0a2e34fbe71ea30b94fe1cedcd167853
jeremy-brooks <jeremy_brooks@hotmail.com>
2013-09-11 19:41:41 +0100

    rename issues

---
+++ src/growersnation/responsive/garden/widget/Gaarden.js
@@ -0,0 +1,39 @@
+/**
+ * @author jeremy.brooks
+ */
+define(["dojo/on",
+		"dojo/_base/declare",
+		"dojo/_base/lang",
+		"dojo/text!./template/plantingCalendar.html",
+		"dijit/_WidgetBase",
+		"dijit/_WidgetsInTemplateMixin",
+		"dijit/_TemplatedMixin"],
+	
+	function(on,
+			 declare,
+			 lang,
+			 template,
+			 _WidgetBase,
+			 _WidgetsInTemplateMixin,
+			 _TemplatedMixin){
+		
+		return declare([_WidgetBase, _WidgetsInTemplateMixin, _TemplatedMixin], {
+			
+			templateString: template,
+			
+			crop: undefined,
+			imgSrc: "",
+			
+			planting: {
+				startDate: undefined,
+				endDate: undefined
+			},
+			
+			harvesting: {
+				startDate: undefined,
+				endDate: undefined
+			}
+			
+		});
+	}
+);
